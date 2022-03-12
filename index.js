b824caf7b386d79437a6c312cb11202e9b2ca953
Mary Rankin <mary-rankin@outlook.com>
2022-03-12 16:59:58 -0500

    added search page and functionality to search for a city from db

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const oracledb = require('oracledb');
 require('dotenv').config();
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
+let connection;
 
 try {
     oracledb.initOracleClient({libDir: process.env.DB_FILEPATH});
@@ -19,29 +23,28 @@ try {
     process.exit(1);
   }
 
+  
+
 async function run() {
-    let connection;
+    //let connection;
 
     try {
         connection = await oracledb.getConnection({user: process.env.DB_USERNAME, password: process.env.DB_PASSWORD, connectionString: "//oracle.cise.ufl.edu/orcl"});
 
         console.log("Successfully connected to Oracle Database");
 
-        //await connection.execute("CREATE TABLE testtable (col1 VARCHAR(20), col2 VARCHAR(25))");
+        //await connection.execute("CREATE TABLE city2 (city_code NUMBER(6) PRIMARY KEY NOT NULL, city_name VARCHAR2(25) NOT NULL, metro VARCHAR2(25), county VARCHAR2(25) NOT NULL, state VARCHAR2(25) NOT NULL, region VARCHAR2(25) NOT NULL, population_rank NUMBER(6) NOT NULL)");
         
-        const stmts = [
-            `INSERT INTO testtable (col1, col2) VALUES ('test1', 'test2')`,
-            `INSERT INTO testtable (col1, col2) VALUES ('test3', 'test4')`
-        ]
+        // const stmts = [
+        //     `INSERT INTO city2 VALUES (6181, 'New York', 'New York', 'Queens', 'NY', 'Northeast', 1)`,
+        //     `INSERT INTO city2 VALUES (12447, 'Los Angeles', 'Los Angeles', 'Los Angeles', 'CA', 'West', 2)`
+        // ]
 
-        for (const s of stmts) {
+        // for (const s of stmts) {
         //    await connection.execute(s);
-        }
+        // }
 
-        //await connection.commit();
-
-        let result = await connection.execute("SELECT * FROM TESTTABLE");
-        console.log(result);
+         //await connection.commit();
     }
 
     catch (err) {
@@ -51,7 +54,7 @@ async function run() {
     finally {
         if (connection) {
             try {
-                await connection.close();
+                //await connection.close();
             }
             catch (err) {
                 console.log(err);
@@ -64,6 +67,55 @@ run();
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+
+app.get('/search', (req, res) => {
+    res.render('search');
+})
+
+app.post('/search', (req, res) => {
+    let cityName = req.body.city;
+
+    res.redirect(`search/${cityName}`);
+})
+
+app.get('/search/:cityName', async (req, res) => {
+    let { cityName } = req.params;
+
+    const cityNameUppercase = cityName.split(" ");
+
+    for (let i = 0; i < cityNameUppercase.length; i++) {
+        cityNameUppercase[i] = cityNameUppercase[i][0].toUpperCase() + cityNameUppercase[i].substr(1);
+    }
+
+    cityName = cityNameUppercase.join(" ");
+
+    let stmt = `SELECT * FROM city2 WHERE city_name='${cityName}'`;
+    let cityInfo = await connection.execute(stmt);
+    console.log(cityInfo)
+
+    if (cityInfo.rows[0] === undefined) {
+        res.render('error');
+    }
+
+    else {
+
+    /* display >1 result if have same city name! */
+
+    /* what if metro is blank? */
+
+    let cityRow = {
+        city_code: cityInfo.rows[0][0],
+        city_name: cityInfo.rows[0][1],
+        metro: cityInfo.rows[0][2],
+        county: cityInfo.rows[0][3],
+        state: cityInfo.rows[0][4],
+        region: cityInfo.rows[0][5],
+        population_rank: cityInfo.rows[0][6]
+    };
+
+    res.render('city', { cityRow });
+}
 })
 
 app.listen(3000, () => {
