6d9ff114a3120f6ccde771a769b9e94a37127f8f
Mary Rankin <mary-rankin@outlook.com>
2022-04-15 19:10:00 -0400

    cleaned up unnecessary code/view files

--- index.js
+++ index.js
@@ -49,43 +49,15 @@ try {
     process.exit(1);
   }
 
-  
-
 async function run() {
-    //let connection;
-
     try {
         connection = await oracledb.getConnection({user: process.env.DB_USERNAME, password: process.env.DB_PASSWORD, connectionString: "//oracle.cise.ufl.edu/orcl"});
 
         console.log("Successfully connected to Oracle Database");
-
-        //await connection.execute("CREATE TABLE city2 (city_code NUMBER(6) PRIMARY KEY NOT NULL, city_name VARCHAR2(25) NOT NULL, metro VARCHAR2(25), county VARCHAR2(25) NOT NULL, state VARCHAR2(25) NOT NULL, region VARCHAR2(25) NOT NULL, population_rank NUMBER(6) NOT NULL)");
-        
-        // const stmts = [
-        //     `INSERT INTO city2 VALUES (6181, 'New York', 'New York', 'Queens', 'NY', 'Northeast', 1)`,
-        //     `INSERT INTO city2 VALUES (12447, 'Los Angeles', 'Los Angeles', 'Los Angeles', 'CA', 'West', 2)`
-        // ]
-
-        // for (const s of stmts) {
-        //    await connection.execute(s);
-        // }
-
-         //await connection.commit();
     }
 
     catch (err) {
         console.log(err);
-    }
-
-    finally {
-        if (connection) {
-            try {
-                //await connection.close();
-            }
-            catch (err) {
-                console.log(err);
-            }
-        }
     }
 }
 
@@ -95,7 +67,6 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-//change to main query page
 app.get('/complexqueries', (req, res) => {
     res.render('complexQueries');
 })
@@ -109,44 +80,6 @@ app.get('/numtuples', async (req, res) => {
     req.flash('success', `Total tuples in database: ${numTuples}`);
     res.redirect('/complexqueries');
     return;
-})
-
-//delete but keep as sample for querying db
-app.get('/search/:cityName', async (req, res) => {
-    let { cityName } = req.params;
-
-    const cityNameUppercase = cityName.split(" ");
-
-    for (let i = 0; i < cityNameUppercase.length; i++) {
-        cityNameUppercase[i] = cityNameUppercase[i][0].toUpperCase() + cityNameUppercase[i].substr(1);
-    }
-
-    cityName = cityNameUppercase.join(" ");
-
-    let stmt = `SELECT * FROM akonate.city1 WHERE "City Name"='${cityName}'`;
-    let cityInfo = await connection.execute(stmt);
-
-    if (cityInfo.rows[0] === undefined) {
-        let errorMsg = "City Not Found";
-        res.render('citySearchError', { errorMsg });
-    }
-
-    else {
-
-    /* display >1 result if have same city name! */
-
-    let cityRow = {
-        city_code: cityInfo.rows[0][0],
-        city_name: cityInfo.rows[0][1],
-        metro: cityInfo.rows[0][2],
-        county: cityInfo.rows[0][3],
-        state: cityInfo.rows[0][4],
-        region: cityInfo.rows[0][5],
-        population_rank: cityInfo.rows[0][6]
-    };
-
-    res.render('city', { cityRow });
-}
 })
 
 app.get('/query1', (req, res) => {
